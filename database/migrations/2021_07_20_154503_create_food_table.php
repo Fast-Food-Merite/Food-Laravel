15cfb9810e700c5c834b03a5584ad4e4a14d99a8
Merite15 <86039889+Merite15@users.noreply.github.com>
2022-05-19 09:03:09 +0200

    insert data in food pizza and chefs

--- database/migrations/2021_07_20_154503_create_food_table.php
+++ database/migrations/2021_07_20_154503_create_food_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateFoodTable extends Migration
@@ -23,6 +24,23 @@ class CreateFoodTable extends Migration
             $table->foreign('category_id')->references('id')->on('categories');
             $table->timestamps();
         });
+
+        DB::table('food')->insert([
+            [
+                'name' => 'Pizza Bolognaise',
+                'description' => 'Venez a Bologne',
+                'price' => '1000',
+                'image' => 'https://www.freeiconspng.com/img/19323',
+                'category_id' => 1,
+            ],
+            [
+                'name' => 'Pizza Burger',
+                'description' => 'Un melange de saveur',
+                'price' => '2000',
+                'image' => 'https://www.freeiconspng.com/img/19323',
+                'category_id' => 1,
+            ]
+        ]);
     }
 
     /**
--- database/migrations/2021_08_27_060924_create_chefs_table.php
+++ database/migrations/2021_08_27_060924_create_chefs_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateChefsTable extends Migration
@@ -17,9 +18,27 @@ class CreateChefsTable extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->string('role')->unique();
-            $table->char('image');
+            $table->string('image');
             $table->timestamps();
         });
+
+        DB::table('chefs')->insert([
+            [
+                'name' => 'Eric',
+                'role' => 'Chef',
+                'image' => 'https://www.shutterstock.com/fr/image-photo/african-american-chef-uniform-on-dark-556163941',
+            ],
+            [
+                'name' => 'Joanna',
+                'description' => 'Assistante',
+                'image' => 'https://www.shutterstock.com/fr/image-photo/smiling-african-american-woman-wearing-apron-1916346464',
+            ],
+            [
+                'name' => 'Jean',
+                'description' => 'Commis',
+                'image' => 'https://www.shutterstock.com/fr/image-photo/cooking-profession-people-concept-happy-male-1426606880',
+            ],
+        ]);
     }
 
     /**
